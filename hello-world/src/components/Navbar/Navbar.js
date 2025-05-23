6cfa39786044ac9d551548594a198a929d26e650
AnveshSrivastava <anveshsrivastava9@gmail.com>
2025-05-23 09:42:24 +0530

    Navbar

--- hello-world/src/components/Navbar/Navbar.js
+++ hello-world/src/components/Navbar/Navbar.js
@@ -1,10 +1,10 @@
 import React from 'react'
-
-export default function Navbar() {
+import PropTypes from 'prop-types';
+export default function Navbar(props) {
   return (
     <div className='container'>
         <nav className="navbar navbar-expand-lg navbar-light bg-light">
-  <a className="navbar-brand" href="#">Navbar</a>
+  <a className="navbar-brand" href="#">{props.title}</a>
   <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
     <span className="navbar-toggler-icon"></span>
   </button>
@@ -18,16 +18,28 @@ export default function Navbar() {
         <a className="nav-link" href="#">Contact</a>
       </li>
       <li className="nav-item">
-        <a className="nav-link" href="#">Aboust Us</a>
+        <a className="nav-link" href="#">{props.aboutText}</a>
       </li>
       
     </ul>
+    {props.searchBar?
     <form className="form-inline my-2 my-lg-0">
       <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
       <button className="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
-    </form>
+    </form>:"No Search Bar"}
   </div>
 </nav>
     </div>
   )
+}
+
+Navbar.propTypes = {
+  title: PropTypes.string,
+  aboutText: PropTypes.string
+}
+
+Navbar.defaultProps ={
+  title: "Set title here",
+  aboutText: "about content",
+  searchBar : false
 }

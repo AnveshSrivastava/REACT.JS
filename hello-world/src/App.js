b9d62dad56e980c3ee7baf8776dc75a1c9ffbbc9
AnveshSrivastava <anveshsrivastava9@gmail.com>
2025-05-27 12:21:48 +0530

    TextForm

--- hello-world/src/App.js
+++ hello-world/src/App.js
@@ -1,20 +1,13 @@
-import logo from './logo.svg';
 import './App.css';
 import Navbar from './components/Navbar/Navbar';
 import TextForm from './components/TextForm/TextForm';
-import { useState } from 'react';
 function App() {
-  const handleUpClick = (event) =>{
-    setText((newText)=>{text.toUpperCase()});
-  }
-  const [text, setText] = useState("Enter Text Here");
+  
   return (
     <div className="App">
-      <Navbar title ="TextUtils" aboutText = {1653561} searchBar = {true}/>
+      <Navbar title ="TextUtils" aboutText = "about" searchBar = {true}/>
       <TextForm/>
-      
-      <div className='btn btn.primary' onClick={handleUpClick}>Convert Up to Upper Case</div>
-    </div>
+      </div>
   );
 }
 
--- hello-world/src/components/TextForm/TextForm.js
+++ hello-world/src/components/TextForm/TextForm.js
@@ -1,12 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react';
 
 export default function TextForm() {
+  const [text, setText] = useState("Enter Text Here");
+
+  const handleUpClick = () => {
+    setText(text.toUpperCase());
+  };
+
+  const handleLoClick = () => {
+    setText(text.toLowerCase());
+  };
+
+  const handleChange = (event) => {
+    setText(event.target.value);
+  };
+  const handleExtraSpaces = () => {
+    let newText = text.split(/[ ]+/).join(" ");
+    setText(newText);
+  };
+
   return (
     <div>
-        <div class="mb-3">
-<label for="exampleFormControlTextarea1" class="form-label">Example textarea</label>
-  <textarea class="form-control" id="exampleFormControlTextarea1" rows="3"></textarea>
-</div>
+      <div className="mb-3">
+        <label htmlFor="exampleFormControlTextarea1" className="form-label">
+          Example textarea
+        </label>
+        <textarea
+          className="form-control"
+          id="exampleFormControlTextarea1"
+          rows="8"
+          value={text}
+          onChange={handleChange}
+        />
+        <button className="btn btn-primary my-3" onClick={handleUpClick}>
+          Convert to Uppercase
+        </button>
+        <button className="btn btn-primary my-3 mx-3" onClick={handleLoClick}>
+          Convert to Lowercase
+        </button>
+        <button className='btn btn-primary my-3 mx-3' onClick={() => setText("")}> Clear Text</button>
+        <button className='btn btn-primary my-3 mx-3' onClick={handleExtraSpaces}> Remove Extra Spaces</button>
+      </div>
     </div>
-  )
+  );
 }
